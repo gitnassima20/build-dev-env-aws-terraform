b244fdca69d1e8b0cbde9f88fc8e5c940a1f4b06
gitnassima20 <nassimabaroudi8@gmail.com>
2024-09-28 19:44:36 +0100

    Configured Terraform provider and first resource VPC

---
+++ providers.tf
@@ -0,0 +1,13 @@
+terraform {
+    required_providers {
+      aws = {
+        source  = "hashicorp/aws"
+      }
+    }
+}
+
+provider "aws" {
+  region                  = "eu-west-3"
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                 = "vscode"
+}
