e5c235d7d82b157416f75b560a6f231f75560d6b
gitnassima20 <nassimabaroudi8@gmail.com>
2024-09-29 21:36:46 +0100

    Format main using "Terraform fmt" and added a igw ressource

--- main.tf
+++ main.tf
@@ -1,20 +1,28 @@
 resource "aws_vpc" "mtc_vpc" {
-    cidr_block = "10.123.0.0/16"
-    enable_dns_hostnames = true
-    enable_dns_support = true
+  cidr_block           = "10.123.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
-    tags = {
-        Name = "dev"
-    }
+  tags = {
+    Name = "dev"
+  }
 }
 
 resource "aws_subnet" "mtc_public_subnet" {
-    vpc_id = aws_vpc.mtc_vpc.id
-    cidr_block = "10.123.1.0/24"
-    map_public_ip_on_launch = true
-    availability_zone = "eu-west-3a"
+  vpc_id                  = aws_vpc.mtc_vpc.id
+  cidr_block              = "10.123.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "eu-west-3a"
 
-    tags = {
-        Name = "dev-public"
-    }
+  tags = {
+    Name = "dev-public"
+  }
+}
+
+resource "aws_internet_gateway" "mtc_internet_gateway" {
+  vpc_id = aws_vpc.mtc_vpc.id
+
+  tags = {
+    Name = "dev-igw"
+  }
 }
